e49e30a7bcd3e281371431e0ef5c5e1102bc5c22
Jason Panella <jpanella@amazon.com>
2025-12-31 16:05:49 -0800

    Initial Commit

---
+++ You-Toss/You_TossApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_TossApp.swift
+//  You-Toss
+//
+//  Created by Panella, Jason on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct You_TossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
