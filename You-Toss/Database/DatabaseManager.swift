1a93e6337610d94a532751082e822259548c1d2f
Jason Panella <jpanella@amazon.com>
2025-12-31 16:31:40 -0800

    Added firebase configuration

---
+++ You-Toss/Database/DatabaseManager.swift
@@ -0,0 +1,7 @@
+//
+//  DatabaseManager.swift
+//  You-Toss
+//
+//  Created by Panella, Jason on 12/31/25.
+//
+
--- You-Toss/You_TossApp.swift
+++ You-Toss/You_TossApp.swift
@@ -5,10 +5,16 @@
 //  Created by Panella, Jason on 12/31/25.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct You_TossApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+      }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
